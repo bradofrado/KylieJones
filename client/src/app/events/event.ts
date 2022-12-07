60ec5ef3ec83af33805b40372471ed04a4c5a4b1
Braydon Jones <bradofrado@gmail.com>
2022-12-07 10:26:22 -0700

    Added IEvent interface and the event-list component dynamically displays events based on the data

---
+++ client/src/app/events/event.ts
@@ -0,0 +1,6 @@
+export interface IEvent {
+    img: string,
+    _id: string,
+    name: string,
+    description: string
+}
--- client/src/app/events/events-list/events-list.component.ts
+++ client/src/app/events/events-list/events-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IEvent } from '../event';
 
 @Component({
   selector: 'app-events-list',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./events-list.component.css']
 })
 export class EventsListComponent {
-    
+    events: IEvent[] = [
+        {
+            img: "assets/ChalkItUp.jpeg",
+            name: "Chalk it Up",
+            description: "A fun time",
+            _id: "0"
+        },
+        {
+            img: "assets/ChalkItUp2.jpeg",
+            name: "Another Chalk",
+            description: "More yes please!",
+            _id: "1"
+        }
+    ];
 }
